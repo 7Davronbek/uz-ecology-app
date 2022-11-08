e390686082dd89331028051c78c87734675da914
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-11-08 15:30:09 +0500

    uz-ecology-app

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <div className='Header'>
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-6">
+                    <h1>Tabiatdagi barcha narsalar biz uchun xizmat qiladi</h1>
+                    <Link to='/'>ro'yxatdan o'tish</Link>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Header from '../components/Header'
 import Navbar from '../components/Navbar'
 
 const Main = () => {
     return (
         <>
             <Navbar />
+            <Header />
         </>
     )
 }
