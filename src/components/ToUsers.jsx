a0de297872eb239212d9b7f047fc0e50be9a3caa
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-11-08 15:41:43 +0500

    uz-ecology-app

---
+++ src/components/ToUsers.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const ToUsers = () => {
+    return (
+        <div className='ToUsers'>
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                        <h1>Foydalanuvchilarga</h1>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col-lg-4 col-md-6 mb-4">
+                        <div className="cards">
+                            <div className="img"><img src="assets/image/toUsers1.png" alt="" /></div>
+                            <div className="cardsBody">
+                                <h2>Buyurtmachilarga</h2>
+                                <h3>Davlat xaridlarining barcha turlari bo‘yicha buyurtmachilarga davlat xaridlari bo‘yicha maxsus portal haqida ma’lumot.</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ToUsers
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/Header'
 import Navbar from '../components/Navbar'
+import ToUsers from '../components/ToUsers'
 
 const Main = () => {
     return (
         <>
             <Navbar />
             <Header />
+            <ToUsers />
         </>
     )
 }
