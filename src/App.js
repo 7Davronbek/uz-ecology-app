a83c181ba6332adeab96a9f414d93fa405daf44e
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-11-08 15:02:13 +0500

    uz-ecology-app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { HashRouter, Route, Routes } from 'react-router-dom'
+import Main from './pages/Main'
 
 const App = () => {
     return (
         <>
-
+            <HashRouter>
+                <Routes>
+                    <Route path='/' element={<Main />} />
+                </Routes>
+            </HashRouter>
         </>
     )
 }
