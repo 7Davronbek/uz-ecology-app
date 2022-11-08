155f85a47e53f1e1ce80c7ae441e8aed563583b4
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-11-08 16:03:50 +0500

    uz-ecology-app

---
+++ src/components/News.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const News = () => {
+  return (
+    <div className='News'>
+        <div className="container">
+            <div className="row">
+                <div className="col-12">
+                    <h1>yANGILIKLAR</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default News
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../components/Header'
 import Navbar from '../components/Navbar'
+import News from '../components/News'
 import ToUsers from '../components/ToUsers'
 
 const Main = () => {
@@ -9,6 +10,7 @@ const Main = () => {
             <Navbar />
             <Header />
             <ToUsers />
+            <News />
             <div className="vh-100"></div>
         </>
     )
