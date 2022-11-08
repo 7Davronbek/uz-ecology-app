4fd5203406650b00b84c809286cb300b8b400954
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-11-08 14:56:43 +0500

    uz-ecology-app

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='Footer'>
+        <div className="container">
+            <div className="row">
+                <div className="col-12">
+                    Footer
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='Navbar'>
+        <div className="container">
+            <div className="row">
+                <div className="col-12">
+                    Navabr
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
