7168ef8f9bc616bece18315b72791cacfeefe2dd
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-12-14 13:55:59 +0500

    uz-ecology-app

--- src/components/user/UserLayout.jsx
+++ src/components/user/UserLayout.jsx
@@ -69,11 +69,12 @@ const UserLayout = (props) => {
             <div className='row  userBottom'>
 
                 <div className="col-lg-3 px-3 bottomLeft">
-                    <h6> OOO YASHIL DARAXT</h6>
+                    <div>
+                        <h6> OOO YASHIL DARAXT</h6>
 
-                    <Accordion className='service__list w-100 border2' open={open} toggle={toggle}>
+                        <Accordion className='service__list w-100 border2' open={open} toggle={toggle}>
 
-                        {/* <AccordionItem className='wrap'>
+                            {/* <AccordionItem className='wrap'>
                             <AccordionHeader targetId='1'>
                                 <h5></h5>
                             </AccordionHeader>
@@ -81,54 +82,57 @@ const UserLayout = (props) => {
                                 <Link className={location.pathname === '/control-panel' ? 'active' : ''} to='/control-panel'>Boshqaruv paneli</Link>
                             </AccordionBody>
                         </AccordionItem> */}
-                        <h6 className={`last ${location.pathname === '/control-panel' ? 'active' : ''}`}><Link to='/control-panel'><span className='icon icon-home'></span>Bosh sahifa</Link></h6>
+                            <h6 className={`last ${location.pathname === '/control-panel' ? 'active' : ''}`}><Link to='/control-panel'><span className='icon icon-home'></span>Bosh sahifa</Link></h6>
 
 
-                        <AccordionItem className='wrap'>
-                            <AccordionHeader targetId='2'>
-                                <h5><span><img src="assets/icon/deal.svg" alt="" /></span> Savdolar va bitimlar</h5>
-                            </AccordionHeader>
-                            <AccordionBody accordionId='2'>
-                                <Link className={location.pathname === '/sales' ? 'active' : ''} to='/sales'>Savdolar</Link>
-                                <Link className={location.pathname === '/transactions' ? 'active' : ''} to='/transactions'>Bitimlar va AKTlar</Link>
-                            </AccordionBody>
-                        </AccordionItem>
+                            <AccordionItem className='wrap'>
+                                <AccordionHeader targetId='2'>
+                                    <h5><span><img src="assets/icon/deal.svg" alt="" /></span> Savdolar va bitimlar</h5>
+                                </AccordionHeader>
+                                <AccordionBody accordionId='2'>
+                                    <Link className={location.pathname === '/sales' ? 'active' : ''} to='/sales'>Savdolar</Link>
+                                    <Link className={location.pathname === '/transactions' ? 'active' : ''} to='/transactions'>Bitimlar va AKTlar</Link>
+                                </AccordionBody>
+                            </AccordionItem>
 
-                        <AccordionItem className='wrap'>
-                            <AccordionHeader targetId='3'>
-                                <h5><span><img src="assets/icon/gardener.svg" alt="" /></span> Ekuvchi tashkilot ma’lumotlari</h5>
-                            </AccordionHeader>
-                            {/* <AccordionBody accordionId='3'>
+                            <AccordionItem className='wrap'>
+                                <AccordionHeader targetId='3'>
+                                    <h5><span><img src="assets/icon/gardener.svg" alt="" /></span> Ekuvchi tashkilot ma’lumotlari</h5>
+                                </AccordionHeader>
+                                {/* <AccordionBody accordionId='3'>
                                 <Link to='/'>Savdolar</Link>
                                 <Link to='/'>Bitimlar</Link>
                             </AccordionBody> */}
-                        </AccordionItem>
+                            </AccordionItem>
 
-                        <AccordionItem className='wrap'>
-                            <AccordionHeader targetId='4'>
-                                <h5><span><img src="assets/icon/location.svg" alt="" /></span> Daraxtlar joylashuvi </h5>
-                            </AccordionHeader>
-                            {/* <AccordionBody accordionId='4'>
+                            <AccordionItem className='wrap'>
+                                <AccordionHeader targetId='4'>
+                                    <h5><span><img src="assets/icon/location.svg" alt="" /></span> Daraxtlar joylashuvi </h5>
+                                </AccordionHeader>
+                                {/* <AccordionBody accordionId='4'>
                                 <Link to='/'>Savdolar</Link>
                                 <Link to='/'>Bitimlar</Link>
                             </AccordionBody> */}
-                        </AccordionItem>
+                            </AccordionItem>
 
-                        <AccordionItem className='wrap wrapLast'>
-                            <AccordionHeader targetId='5'>
-                                <h5><span className='icon icon-file'></span>Hujjatlar </h5>
-                            </AccordionHeader>
-                            <AccordionBody accordionId='5'>
-                                <Link className={` ${location.pathname === '/documents' ? 'active' : ''}`} to='/documents'> Hujjatlar </Link>
-                                <Link className={` ${location.pathname === '/reports' ? 'active' : ''}`} to='/reports'>Xisobotlarim </Link>
-                            </AccordionBody>
-                        </AccordionItem>
+                            <AccordionItem className='wrap wrapLast'>
+                                <AccordionHeader targetId='5'>
+                                    <h5><span className='icon icon-file'></span>Hujjatlar </h5>
+                                </AccordionHeader>
+                                <AccordionBody accordionId='5'>
+                                    <Link className={` ${location.pathname === '/documents' ? 'active' : ''}`} to='/documents'> Hujjatlar </Link>
+                                    <Link className={` ${location.pathname === '/reports' ? 'active' : ''}`} to='/reports'>Xisobotlarim </Link>
+                                </AccordionBody>
+                            </AccordionItem>
 
-                    </Accordion>
+                        </Accordion>
 
-                    {/* <h6 ></h6> */}
-                    {/* <h6 ></h6> */}
-                    <h6 className={`last ${location.pathname === '/settings' ? 'active' : ''}`}><Link to='/settings'><span className='icon icon-setting'></span> Sozlamalar </Link></h6>
+                        {/* <h6 ></h6> */}
+                        {/* <h6 ></h6> */}
+                        <h6 className={`last ${location.pathname === '/settings' ? 'active' : ''}`}><Link to='/settings'><span className='icon icon-setting'></span> Sozlamalar </Link></h6>
+                    </div>
+
+                    <h6 className="logout"><span><img src="assets/icon/logout.svg" alt="" /></span> Chiqish</h6>
 
                 </div>
                 <div className="col-lg-9 px-3 bottomRight">
